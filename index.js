00b01d6f6df8c75d502e04e8b056e8a4ecedb83a
Laurine Maina <mainalaureen58@gmail.com>
2024-04-08 19:52:19 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+  const url = 'http://localhost:3000/users';
+  const data = {
+      name: name,
+      email: email
+  };
+
+  const options = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+  };
+
+  return fetch(url, options)
+      .then(response => {
+          if (!response.ok) {
+              return new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+          document.body.innerHTML += `<p>New ID: ${data.id}</p>`;
+      })
+      .catch(error => {
+          document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+           return submitData;
+
+      });
+
+}
